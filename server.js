abd9755ecd8798782b876da600c1825dae8666bc
hori-c-c <horielcoxim@gmail.com>
2021-11-30 08:58:14 -0400

    Tratando as informações sensíveis

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const roteador = require('./rotas/roteador')
 const mongoose = require('mongoose')
@@ -13,14 +14,15 @@ app.use(express.json())
 app.use('/game', roteador)
 
 
-mongoose.connect('mongodb+srv://horiel:admin@entregasprint2.xcqrc.mongodb.net/bancodaapi?retryWrites=true&w=majority')
+//credenciais do banco de dados
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@entregasprint2.xcqrc.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
 .then(() =>
 {
     console.log('Banco de dados conectado!')
     app.listen(5000, ()=> {console.log('Servidor Rodando!!!')})
 })
 .catch((erro) => console.log(erro))
-
-
-//usuario: horiel   senha: admin
-//mongodb+srv://horiel:admin@entregasprint2.xcqrc.mongodb.net/bancodaapi?retryWrites=true&w=majority
